e98627b8e0517cdaf0fbe29a7d7513a27576f4f6
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-05 21:41:28 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,7 +7,7 @@ class ArtistsController < ApplicationController
     @artist = Artist.find(params[:id])
   end
 
-#preferences 
+#preferences, creation of new artists 
   def new
     if Preference.first && !Preference.first.allow_create_artists
       redirect_to artists_path
